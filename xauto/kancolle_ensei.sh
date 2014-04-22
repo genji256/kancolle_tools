d25abef2a7a795fbc5d765041ad3d2e88e56e08b
genji256 <sosesusisa@gmail.com>
2014-04-23 05:39:03 +0900

    自動遠征スクリプトの補給関数をある程度作成

--- xauto/kancolle_ensei.sh
+++ xauto/kancolle_ensei.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 #-----艦これFlashの左上の座標-----
 KAN_X=450
 KAN_Y=200
@@ -26,17 +27,64 @@ mouseOpe()
 #-----出撃関数-----
 sortie()
 {
-
+	echo "sortie"
 }
 
 #-----補給関数-----
 supply()
 {
+	echo "supply"
+
+	#艦隊番号ボタンの座標
+	rateX=30
+	kantaiY=80
+	kantaiX=165
+	kantai3X=190
+	kantai4X=220
+
+	#艦娘チェックボタンの座標
+	checkKanX=110
+	checkKanY=80
+
+	#補給ボタンの座標
+	supplyBtnX=700
+	supplyBtnY=400
+
+	#補給ボタンをクリックして補給画面へ
+	mouseOpe $SUPPLY_X $SUPPLY_Y
+
+	#3艦隊分繰り返す
+	i=0
+	max=3
+
+	while [ $i -lt $max ]
+	do
+		echo "loop$i"
+		#艦隊番号を指定
+		mouseOpe $((kantaiX+rateX\*i)) $kantaiY
+		sleep 1s
+	
+		#全艦娘にチェックを入れる
+		mouseOpe $checkKanX $checkKanY
+		sleep 1s
+	
+		#補給ボタンをクリック
+		mouseOpe $supplyBtnX $supplyBtnY
+		sleep 1s
+
+		i=$((i+1))
+
+	done
+	
+	sleep 1s
+	#ホーム画面へ戻る
+	mouseOpe 0 0
 
 }
 
 #-----main-----
-mouseOpe $SUPPLY_X $SUPPLY_Y
+supply
+
 
 echo "Done"
 
