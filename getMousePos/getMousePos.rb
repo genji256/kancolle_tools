40f1113bcc0d61a073ba0b9f0c9d94f9e9716f9d
HonoEli <honoeli@HonoEli>
2015-02-14 01:36:45 +0900

    マウスポインタの座標を取得するRubyスクリプトを作成

---
+++ getMousePos/getMousePos.rb
@@ -0,0 +1,65 @@
+#!/usr/bin/env ruby
+
+#---------------------------------------------------------------------------
+#
+#数秒後のマウスポインタの座標を取得する
+#
+#---------------------------------------------------------------------------
+
+class GetMousePos
+
+	#初期化
+	def initialize(num)
+		@s = num	#秒数
+	end
+	
+	#カウントダウン関数
+	def countDown()
+		puts "CountDown!"
+		@s.downto(0) { |num|
+			puts num
+			sleep 1
+		}
+	end
+	
+	#xmouseposコマンド実行関数
+	def xmousepos
+		return %x( /bin/bash './xmousepos.sh')
+	end
+
+	#xmouseposで取得した座標を整理する
+	def clearPos
+		value = xmousepos
+		@pos = value.split(" ")
+		#必要なのは画面全体からみた座標なのでそれ以外は取り除く
+		@pos.delete_at(2)
+		@pos.delete_at(2)
+	end
+
+	#カウントダウンから座標取得まで一連の流れを行う	
+	def start
+		countDown	
+		clearPos
+	end
+
+	#取得した座標を配列で返す
+	def getMousePos
+		return @pos
+	end
+
+	#取得したX座標を返す
+	def getX
+		return @pos[0]
+	end
+
+	#取得したX座標を返す
+	def getY
+		return @pos[1]
+	end
+
+end
+
+gmp = GetMousePos.new(3)
+gmp.start
+pos = gmp.getMousePos
+puts "x:#{pos[0]} y:#{pos[1]}"
