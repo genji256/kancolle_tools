42e9164e53b27b10eba69ea3104d8e5a65aff389
genji256 <sosesusisa@gmail.com>
2015-02-15 03:11:49 +0900

    ほんのちょっとだけ追加

--- autoExpedition.rb
+++ autoExpedition.rb
@@ -6,4 +6,22 @@
 #
 #---------------------------------------------------------------------------
 
-#中身はまだないよ！
+require "./lib/mouseAction.rb"
+require "./lib/positionManager.rb"
+
+#マウス操作オブジェクト
+ma = MouseAction.new()
+#左上の母港
+boko = PositionManager.new(1021, 283, 35, 35)
+#出撃
+syutu = PositionManager.newIni(195, 261)
+
+#出撃画面へ
+ma.move(syutu)
+ma.clickLeft()
+
+#母港へ戻る
+ma.move(boko)
+ma.clickLeft()
+
+
